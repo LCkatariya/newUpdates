76e871a03140bbc630039be3ab5fa23cc7af3ccc
LCkatariya <lalchandkatariya91@gmail.com>
2026-02-03 22:24:01 +0530

    update new commits

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
     <div>hi my name is lalchand saini</div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+
+    
     </p>
   </div>
 `
